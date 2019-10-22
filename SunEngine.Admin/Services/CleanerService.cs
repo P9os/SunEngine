5b02ab535bade983c445cd455c15cd9d6a10dea5
P9oS <sandybaevr@yandex.ru>
2019-10-22 10:44:13 +0500

    +

---
+++ SunEngine.Admin/Services/CleanerService.cs
@@ -0,0 +1,67 @@
+using System.Linq;
+using System.Threading.Tasks;
+using LinqToDB;
+using SunEngine.Core.DataBase;
+
+namespace SunEngine.Admin.Services
+{
+    public interface ICleanerService
+    {
+        Task DeleteMaterial(int idMaterial);
+
+        Task DeleteComment(int idComment);
+
+        Task DeleteAllDeleteMaterials();
+
+        Task DeleteAllDeleteComments();
+    }
+
+    public class CleanerService : ICleanerService
+    {
+        private readonly DataBaseConnection db;
+
+        public CleanerService(
+            DataBaseConnection dataBaseConnection)
+        {
+            db = dataBaseConnection;
+        }
+
+        public async Task DeleteMaterial(int idMaterial)
+        {
+            var material = db.Materials.Where(x => x.Id == idMaterial);
+
+            await material.Select(x => x.Comments).DeleteAsync();
+            await material.DeleteAsync();
+        }
+
+        public async Task DeleteComment(int idComment)
+        {
+            var comment = db.Comments.Where(x => x.Id == idComment).ElementAt(0);
+            var material = comment.Material;
+
+            if (material.LastCommentId == comment.Id)
+            {
+                var newLastComment = material.Comments.OrderBy(x => x.PublishDate).Last();
+                material.LastCommentId = newLastComment.Id;
+                material.LastComment = newLastComment;
+
+                await db.UpdateAsync(material);
+            }
+
+            await db.DeleteAsync(comment);
+        }
+
+        public async Task DeleteAllDeleteMaterials()
+        {
+            var materials = db.Materials.Where(x => x.DeletedDate != null);
+
+            await materials.Select(x => x.Comments).DeleteAsync();
+            await materials.DeleteAsync();
+        }
+
+        public async Task DeleteAllDeleteComments()
+        {
+            await db.Comments.Where(x => x.DeletedDate != null).DeleteAsync();
+        }
+    }
+}
