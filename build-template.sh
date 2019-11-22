abb677518ac8641b75e4ee3a4ef1a6e2f0a39ad2
P9oS <sandybaevr@yandex.ru>
2019-11-22 22:07:49 +0500

    +

--- build-template.sh
+++ build-template.sh
@@ -42,7 +42,7 @@ echo -e "\n${GREEN}Publishing to directory ${PROJECT_NAME} project$ ${NC}\n"
 
 cd "$SOLUTION_PATH"
 
-echo -e "Building backend" 
+echo -e "Building server" 
 if [ dotnet > /dev/null ]; then
     echo -e "\n${GREEN}Publish build ${PROJECT_NAME} ${NC}\n"
     dotnet publish --configuration Release "$SOLUTION_PATH/SunEngine.Cli" --output "$OUTPUT/backend"
@@ -54,20 +54,33 @@ fi
 
 echo -e "\n${GREEN}Building Client ${NC}\n"
 cd "$CLIENT_PATH"
+
+#check on available quasar
+if [ ! quasar > /dev/null ]; then
+    echo -e "\n${RED} Quasar not install please install by command\n${BLUE} npm install -g @quasar/cli "
+    exit 1
+fi
+
+npm install
+cp -r $PWD/src/site-template $PWD/src/site
 quasar build
 
 
-echo  -e "\n${GREEN}Copying Client to wwwroot directory ${NC}\n"
-cp -r "$CLIENT_PATH/dist/spa/." "$BUILD_PATH/wwwroot"
 
 
-echo -e "\n${GREEN}Install node_modules ${NC}\n if it no there ${NC}\n"
-
-if [ ! -d SunEngine/Client/node_modules ]; then
-cd SunEngine/Client && npm install &
-fi
 
 
-echo -e "\n${GREEN}Copy "site-template" to "site" ${NC}\n"
+# echo  -e "\n${GREEN}Copying Client to wwwroot directory ${NC}\n"
+# cp -r "$CLIENT_PATH/dist/spa/." "$BUILD_PATH/wwwroot"
 
-cp -r SunEngine/Client/src/site-template SunEngine/Client/src/site
+
+# echo -e "\n${GREEN}Install node_modules ${NC}\n if it no there ${NC}\n"
+
+# if [ ! -d SunEngine/Client/node_modules ]; then
+# cd SunEngine/Client && npm install &
+# fi
+
+
+# echo -e "\n${GREEN}Copy "site-template" to "site" ${NC}\n"
+
+# cp -r SunEngine/Client/src/site-template SunEngine/Client/src/site
